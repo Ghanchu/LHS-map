8f4d27e93749fa6edd2b87f23a2ce05eefb231cf
iamverynotverysmart <aparvaz@umich.edu>
2023-07-10 22:47:12 -0600

    added the ability to delete the polygons because i am cool that way

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,56 +1,70 @@
 function initMap() {
   document.addEventListener("DOMContentLoaded", () => {
-    class shape {
+    class Shape {
       polygon: { lat: number; lng: number }[];
-      map: any
+      map: any;
+      bermudaTriangles: google.maps.Polygon[];
+
       constructor(map: any) {
         this.polygon = [];
-        this.map = map
+        this.map = map;
+        this.bermudaTriangles = [];
       }
 
-      addpoint(lat: number, lng: number) {
+      addPoint(lat: number, lng: number) {
         this.polygon.push({ lat, lng });
-        if(this.polygon.length >= 3) {
-          this.update(map, lat, lng)
+        if (this.polygon.length >= 3) {
+          this.update(lat, lng);
         }
-      
       }
 
-      update(map: any, lat: number, lng: number) {
-        let top: {lat: number, lng: number, distance: number} [] = [];
-        this.polygon.forEach((item: {lat: number, lng: number}) => {
-          let benchmark = Math.sqrt(Math.pow((lat-item.lat), 2) + Math.pow((lng - item.lng), 2))
+      update(lat: number, lng: number) {
+        let top: { lat: number; lng: number; distance: number }[] = [];
+        this.polygon.forEach((item: { lat: number; lng: number }) => {
+          let benchmark = Math.sqrt(
+            Math.pow(lat - item.lat, 2) + Math.pow(lng - item.lng, 2)
+          );
           const selection = {
-            lat: item.lat, 
-            lng: item.lng, 
-            distance: benchmark
-          }
-          top.push(selection)
-          top.sort((a,b) => a.distance - b.distance);
-          
-        })
-        const chosen: {lat: number, lng: number, distance: number} [] = [
+            lat: item.lat,
+            lng: item.lng,
+            distance: benchmark,
+          };
+          top.push(selection);
+          top.sort((a, b) => a.distance - b.distance);
+        });
+
+        const chosen: { lat: number; lng: number; distance: number }[] = [
           top[0],
           top[1],
-          top[2]
-        ]
+          top[2],
+        ];
 
-        chosen.push(chosen[0])
-        const bermudatriangle = new google.maps.Polygon({
+        chosen.push(chosen[0]);
+        const bermudaTriangle = new google.maps.Polygon({
           paths: chosen,
-          strokeColor: "#FF000", 
+          strokeColor: "#FF000",
           strokeOpacity: 0.8,
           strokeWeight: 2,
           fillColor: "FF000",
-          fillOpacity: 0.35
-          
+          fillOpacity: 0.35,
         });
 
-        bermudatriangle.setMap(map)
+        bermudaTriangle.addListener("customEvent", () => {
+          bermudaTriangle.setMap(null);
+        });
+
+        bermudaTriangle.setMap(this.map);
+        this.bermudaTriangles.push(bermudaTriangle);
+      }
+
+      clearBermudaTriangles() {
+        for (let i = 0; i < this.bermudaTriangles.length; i++) {
+          google.maps.event.trigger(this.bermudaTriangles[i], "customEvent");
+        }
+        this.bermudaTriangles = [];
       }
     }
 
-    
     var uluru = { lat: -25.363, lng: 131.044 };
     var map = new google.maps.Map(
       document.getElementById("map") as HTMLElement,
@@ -60,15 +74,18 @@ function initMap() {
       }
     );
 
-    let main_shape = new shape(map);
-    const meme = () => {
-      console.log("you clicked here");
-    };
+    let mainShape = new Shape(map);
+
     map.addListener("click", (event) => {
       const lat = JSON.stringify(event.latLng.toJSON().lat);
       const lng = JSON.stringify(event.latLng.toJSON().lng);
-      main_shape.addpoint(Number(lat), Number(lng));
-      console.log(main_shape.polygon)
+      mainShape.addPoint(Number(lat), Number(lng));
+      console.log(mainShape.polygon);
+    });
+
+    const clear = document.getElementById("new_shape") as HTMLButtonElement;
+    clear.addEventListener("click", () => {
+      mainShape.clearBermudaTriangles();
     });
   });
 }
